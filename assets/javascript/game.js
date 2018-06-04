d61a2326f3f1096ced5375063aef9027e146b596
Ross <paddon.ross@gmail.com>
2018-06-04 10:48:39 -0700

    got Wins part working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,31 @@
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
 //rounds up to 27 when it's 26.5 or higher, else it would sometimes round up on 27 and give undefined
 var randomnum= Math.round((Math.random()*26));
 var randomLetter = alphabet[randomnum];
 console.log('random letter is ' + randomLetter)
-
-
+var wins = 0;
+var lose = 0;
+var attempts = 0;
 function UserKey(event){
  var key = event.key;
  
  if(key == randomLetter){
-     
+     wins++;
     
-    console.log('win')
+    console.log('win');
+    document.getElementById("wins").innerHTML = wins;
 
  }
  else{
      console.log('loss')
+     lose++
+     document.getElementById("wins").innerHTML = wins;
  }
+ 
+ console.log(wins);
+ console.log(lose);
+
 
 }
  
